b9b0fa4ee452fa49946c929544dbcd83690fa12a
petermulsh <petermulsh@yahoo.com>
2024-05-01 18:44:22 -0400

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Peter Mulshine on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
